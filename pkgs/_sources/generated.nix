7fe9089ce5cd09e1dc7666c6586c490d4e621aee
naitokosuke <102337893+naitokosuke@users.noreply.github.com>
2026-09-18 23:03:10 +0000

    chore: update nvfetcher sources

--- pkgs/_sources/generated.nix
+++ pkgs/_sources/generated.nix
@@ -67,10 +67,10 @@
   };
   vite-plus-darwin-arm64 = {
     pname = "vite-plus-darwin-arm64";
-    version = "0.3.2";
+    version = "0.3.3";
     src = fetchurl {
-      url = "https://registry.npmjs.org/@voidzero-dev/vite-plus-cli-darwin-arm64/-/vite-plus-cli-darwin-arm64-0.3.2.tgz";
-      sha256 = "sha256-2kwbpgdmN1YrZne04p3/ClEYKhsbo4ZFCN9QCu6nKHk=";
+      url = "https://registry.npmjs.org/@voidzero-dev/vite-plus-cli-darwin-arm64/-/vite-plus-cli-darwin-arm64-0.3.3.tgz";
+      sha256 = "sha256-opYlp3O8j+9N5ENKZ6JPNISbPCwBWeqHqFTIhT7JR1Q=";
     };
   };
   vize-darwin-arm64 = {
